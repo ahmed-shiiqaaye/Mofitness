e797c53a8165aa9342f759a4a46347fae92c9d77
ahmed-shiiqaaye <me.shiqaye@gmail.com>
2022-03-19 21:32:38 +0200

    renaming the comments

--- js/app.js
+++ js/app.js
@@ -4,3 +4,35 @@ let links = document.getElementById('links');
 menuBtn.addEventListener('click',function(){
    links.classList.toggle('active')
 })
+
+// filter image
+let myBtn = document.querySelector('.btns')
+let filterImages = document.querySelectorAll('.img');
+
+window.onload = () => {
+    myBtn.addEventListener('click',function(e){
+        let selectedItem = e.target.classList;
+        if(selectedItem.contains("butn")){
+            // changing the active button
+            let activeBtn = myBtn.querySelector('.active');
+            activeBtn.classList.remove('active')
+            e.target.classList.add('active')
+            // getting the data-attribute
+            let filterName = e.target.getAttribute('data-name');
+            // console.log(filterName)
+            // filtering the images as data-name
+            filterImages.forEach(image =>{
+                let filImg =image.getAttribute('data-name');
+                // if the user selected buton data-name is equal to 
+                // the image data-name or 'all'
+                if(filImg == filterName || filterName == "all"){
+                    image.classList.remove('hide')
+                    image.classList.add('show')
+                }else{
+                    image.classList.remove('show')
+                    image.classList.add('hide')
+                }
+            });
+        }
+    });
+}
